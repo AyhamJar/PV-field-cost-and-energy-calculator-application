94d9617a1ce7d91d6d93e8616d5d1adf571191c2
AyhamJar <ayham.aaljarrah@gmail.com>
2023-06-19 22:12:03 +0400

    removed pages and added button

--- webapp/PVapp/views.py
+++ webapp/PVapp/views.py
@@ -3,14 +3,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, "index.html")
 
-def mono(request):
-    return render(request, "mono.html")
-
-def poly(request):
-    return render(request, "poly.html")
-
-def thin(request):
-    return render(request, "thin.html")
-
 def calculate(request):
     return render(request, "calculate.html")
--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -5,8 +5,5 @@ from PVapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('mono/', views.mono),
-    path('poly/', views.poly),
-    path('thin/', views.thin),
     path('calculate/', views.calculate),
 ]
